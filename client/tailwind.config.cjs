8b73f2625fbd0844e596f28c09cef73df1e029ee
Dylan L <dylancblarocque@gmail.com>
2023-04-26 09:50:39 -0700

    add customer color pallete

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}'
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        custom1: '#00072d',
+        custom2: '#001e5e',
+        custom3: '#0a2472',
+        custom4: '#0e6ba8',
+        custom5: '#a6e1fa',
+      },
+    },
   },
-  plugins: []
-}
+  plugins: [],
+};
