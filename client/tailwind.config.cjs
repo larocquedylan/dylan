9bc5a0aa20a8ab5e721490a34e6f0f63dbea7e77
Dylan L <dylancblarocque@gmail.com>
2023-04-26 17:11:38 -0700

    add custom width size for lg

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
@@ -10,7 +11,13 @@ module.exports = {
         custom4: '#0e6ba8',
         custom5: '#a6e1fa',
       },
+      fontFamily: {
+        sans: ['Inter'],
+      },
+      maxWidth: {
+        custom: '900px', // Add this line
+      },
+      plugins: [],
     },
   },
-  plugins: [],
 };
