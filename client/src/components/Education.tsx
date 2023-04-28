d6924cc33f42e690009f7c74dd0995683a6bf753
Dylan L <dylancblarocque@gmail.com>
2023-04-28 08:37:22 -0700

    add url styling and paragraph formatting

---
+++ client/src/components/Education.tsx
@@ -0,0 +1,93 @@
+import React, { useState } from 'react';
+
+interface EducationProps {
+  key: string;
+  title: string;
+  description: string;
+  dates: string;
+}
+
+const Education: React.FC<EducationProps> = ({
+  key,
+  title,
+  dates,
+  description,
+}) => {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleClick = () => {
+    setExpanded(!expanded);
+  };
+
+  // Split description into paragraphs
+  const paragraphs = description.split(/<br\/>|\\n\\n/);
+
+  // create alias for urls
+  const urlAliases = {
+    'https://mybcom.sauder.ubc.ca/courses-money-enrolment/courses/comm-466':
+      'New Venture Design',
+    'https://mybcom.sauder.ubc.ca/courses-money-enrolment/courses/comm-483':
+      'General Leadership & Management',
+    'https://epicreact.dev': 'EpicReact',
+    'https://brainstation.io/course/online/remote-web-development-bootcamp':
+      'BrainStation',
+    'https://github.com/larocquedylan/beats-for-bits-api': 'Beats for Bits',
+  };
+
+  // convert links to anchor tags
+  const convertUrlsToAnchors = (text: string) => {
+    const urlRegex =
+      /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi;
+    return text.replace(urlRegex, (url) => {
+      const alias = urlAliases[url] || url;
+      return `<a href="${url}" target="_blank" rel="noopener noreferrer" class="text-blue-600 font-bold">${alias}</a>`;
+    });
+  };
+
+  return (
+    <div className='flex flex-col'>
+      <div className='flex items-center justify-between p-4 pr-0 bg-custom1'>
+        <h2 className='text-custom5 text-sm'>{title}</h2>
+        <button onClick={handleClick} className='focus:outline-none'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            className={`transform transition-transform duration-200 ${
+              expanded ? 'rotate-180' : ''
+            }`}
+            width={24}
+            height={24}
+            viewBox='0 0 24 24'
+            strokeWidth='1.5'
+            stroke='#a6e1fa'
+            fill='none'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+          >
+            <path stroke='none' d='M0 0h24v24H0z' />
+            <polyline points='6 9 12 15 18 9' />
+          </svg>
+        </button>
+      </div>
+      {expanded && (
+        <div
+          className='ml-4 p-4 bg-custom2 text-custom5 rounded transition duration-500 ease-in-out max-h-0 overflow-hidden'
+          style={{ maxHeight: expanded ? '100vh' : '0' }}
+        >
+          <p className='mb-4 text-sm'> {dates}</p>
+          {paragraphs.map((paragraph, index) => {
+            const htmlContent = convertUrlsToAnchors(paragraph);
+            return (
+              <p
+                key={index}
+                className='mb-2 text-sm'
+                dangerouslySetInnerHTML={{ __html: htmlContent }}
+              ></p>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Education;
