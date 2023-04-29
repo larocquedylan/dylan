e0888f44fd876999464358ff21b50bf9b6f86151
Dylan L <dylancblarocque@gmail.com>
2023-04-29 06:25:47 -0700

    refactor with ExpandableSection component

--- client/src/components/AcroBuzz.tsx
+++ client/src/components/AcroBuzz.tsx
@@ -1,5 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import convertUrlsToAnchors from '../utils/convertUrqlToAnchor';
+import paragraphBreak from '../utils/paragraphBreak';
+import ExpandableSection from './ExpandableSection';
 
 interface AcroBuzzProps {
   key: string;
@@ -14,14 +16,7 @@ const AcroBuzz: React.FC<AcroBuzzProps> = ({
   dates,
   description,
 }) => {
-  const [expanded, setExpanded] = useState(false);
-
-  const handleClick = () => {
-    setExpanded(!expanded);
-  };
-
-  // Split description into paragraphs
-  const paragraphs = description.split(/<br\/>|\\n\\n/);
+  const paragraphs = paragraphBreak(description);
 
   const urlAliases = {
     'https://github.com/larocquedylan/AcroBuzz-Server': 'Server repo',
@@ -39,51 +34,21 @@ const AcroBuzz: React.FC<AcroBuzzProps> = ({
   };
 
   return (
-    <div className='flex flex-col'>
-      <div className='flex items-center justify-between p-4 pr-0 bg-custom1'>
-        <h2 className='text-custom5 text-sm'>{title}</h2>
-        <button onClick={handleClick} className='focus:outline-none'>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            className={`transform transition-transform duration-200 ${
-              expanded ? 'rotate-180' : ''
-            }`}
-            width={24}
-            height={24}
-            viewBox='0 0 24 24'
-            strokeWidth='1.5'
-            stroke='#a6e1fa'
-            fill='none'
-            strokeLinecap='round'
-            strokeLinejoin='round'
-          >
-            <path stroke='none' d='M0 0h24v24H0z' />
-            <polyline points='6 9 12 15 18 9' />
-          </svg>
-        </button>
-      </div>
-      {expanded && (
-        <div
-          className='ml-4 p-4 bg-custom2 text-custom5 rounded transition duration-500 ease-in-out max-h-0 overflow-hidden'
-          style={{ maxHeight: expanded ? '100vh' : '0' }}
-        >
-          <p className='mb-4 text-sm'> {dates}</p>
-          {paragraphs.map((paragraph, index) => {
-            const htmlContent = convertUrlsToAnchors(
-              boldKeywords(paragraph),
-              urlAliases
-            );
-            return (
-              <p
-                key={index}
-                className='mb-2 text-sm'
-                dangerouslySetInnerHTML={{ __html: htmlContent }}
-              ></p>
-            );
-          })}
-        </div>
-      )}
-    </div>
+    <ExpandableSection title={title} dates={dates}>
+      {paragraphs.map((paragraph, index) => {
+        const htmlContent = convertUrlsToAnchors(
+          boldKeywords(paragraph),
+          urlAliases
+        );
+        return (
+          <p
+            key={index}
+            className='mb-2 text-sm'
+            dangerouslySetInnerHTML={{ __html: htmlContent }}
+          ></p>
+        );
+      })}
+    </ExpandableSection>
   );
 };
 
--- client/src/components/Education.tsx
+++ client/src/components/Education.tsx
@@ -1,5 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import convertUrlsToAnchors from '../utils/convertUrqlToAnchor';
+import paragraphBreak from '../utils/paragraphBreak';
+import ExpandableSection from './ExpandableSection';
 
 interface EducationProps {
   key: string;
@@ -14,16 +16,8 @@ const Education: React.FC<EducationProps> = ({
   dates,
   description,
 }) => {
-  const [expanded, setExpanded] = useState(false);
+  const paragraphs = paragraphBreak(description);
 
-  const handleClick = () => {
-    setExpanded(!expanded);
-  };
-
-  // Split description into paragraphs
-  const paragraphs = description.split(/<br\/>|\\n\\n/);
-
-  // create alias for urls
   const urlAliases = {
     'https://mybcom.sauder.ubc.ca/courses-money-enrolment/courses/comm-466':
       'New Venture Design',
@@ -36,48 +30,18 @@ const Education: React.FC<EducationProps> = ({
   };
 
   return (
-    <div className='flex flex-col'>
-      <div className='flex items-center justify-between p-4 pr-0 bg-custom1'>
-        <h2 className='text-custom5 text-sm'>{title}</h2>
-        <button onClick={handleClick} className='focus:outline-none'>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            className={`transform transition-transform duration-200 ${
-              expanded ? 'rotate-180' : ''
-            }`}
-            width={24}
-            height={24}
-            viewBox='0 0 24 24'
-            strokeWidth='1.5'
-            stroke='#a6e1fa'
-            fill='none'
-            strokeLinecap='round'
-            strokeLinejoin='round'
-          >
-            <path stroke='none' d='M0 0h24v24H0z' />
-            <polyline points='6 9 12 15 18 9' />
-          </svg>
-        </button>
-      </div>
-      {expanded && (
-        <div
-          className='ml-4 p-4 bg-custom2 text-custom5 rounded transition duration-500 ease-in-out max-h-0 overflow-hidden'
-          style={{ maxHeight: expanded ? '100vh' : '0' }}
-        >
-          <p className='mb-4 text-sm'> {dates}</p>
-          {paragraphs.map((paragraph, index) => {
-            const htmlContent = convertUrlsToAnchors(paragraph, urlAliases);
-            return (
-              <p
-                key={index}
-                className='mb-2 text-sm'
-                dangerouslySetInnerHTML={{ __html: htmlContent }}
-              ></p>
-            );
-          })}
-        </div>
-      )}
-    </div>
+    <ExpandableSection title={title} dates={dates}>
+      {paragraphs.map((paragraph, index) => {
+        const htmlContent = convertUrlsToAnchors(paragraph, urlAliases);
+        return (
+          <p
+            key={index}
+            className='mb-2 text-sm'
+            dangerouslySetInnerHTML={{ __html: htmlContent }}
+          ></p>
+        );
+      })}
+    </ExpandableSection>
   );
 };
 
