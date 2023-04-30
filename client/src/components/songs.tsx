d9ccbf8be519c754c35a99dd048f404f27506119
Dylan L <dylancblarocque@gmail.com>
2023-04-30 08:44:52 -0700

    stream working

--- client/src/components/songs.tsx
+++ client/src/components/songs.tsx
@@ -7,20 +7,21 @@ interface SongsProps {
   id: number;
   title: string;
   fileURL: string;
+  download: string;
 }
 
-const Songs: React.FC<SongsProps> = ({ id, title, fileURL }) => {
-  const downloadURL = `http://localhost:8080/songs/download/${id}`;
+const Songs: React.FC<SongsProps> = ({ id, title, fileURL, download }) => {
+  const downloadURL = `http://localhost:8080/songs/download/${title}.wav`;
 
-  console.log(downloadURL);
-  console.log(id);
+  //   console.log(downloadURL);
+  //   console.log(id);
 
   return (
     <ExpandableSection title={title} dates={''}>
       <div className='flex items-center'>
         <AudioPlayer src={fileURL} autoPlay={false} />
         <a
-          href={downloadURL}
+          href={download}
           className='ml-4 px-3 py-1 text-sm bg-custom3 text-custom1 rounded hover:bg-custom2 transition-colors duration-300'
         >
           Download
