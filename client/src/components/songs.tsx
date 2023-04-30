4cb6a40831e55151205d3c74d63ddd08dcba0421
Dylan L <dylancblarocque@gmail.com>
2023-04-29 17:44:37 -0700

    song component

---
+++ client/src/components/songs.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import ExpandableSection from './ExpandableSection';
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css'; // Import the required styles
+
+interface SongsProps {
+  id: number;
+  title: string;
+  fileURL: string;
+}
+
+const Songs: React.FC<SongsProps> = ({ id, title, fileURL }) => {
+  const downloadURL = `http://localhost:8080/songs/download/${id}`;
+
+  console.log(downloadURL);
+  console.log(id);
+
+  return (
+    <ExpandableSection title={title} dates={''}>
+      <div className='flex items-center'>
+        <AudioPlayer src={fileURL} autoPlay={false} />
+        <a
+          href={downloadURL}
+          className='ml-4 px-3 py-1 text-sm bg-custom3 text-custom1 rounded hover:bg-custom2 transition-colors duration-300'
+        >
+          Download
+        </a>
+      </div>
+    </ExpandableSection>
+  );
+};
+
+export default React.memo(Songs);
