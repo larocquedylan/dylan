641c1eae54f027d1f60405d1a85487783d451a7c
larocquedylan <dylancblarocque@gmail.com>
2024-01-21 22:16:11 -0800

    remove some components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,4 @@
 import './App.css'
-// import Bio from './components/Bio';
-import BodyNav from './components/BodyNav'
 import NavBar from './components/NavBar'
 import Resume from './pages/Resume'
 
@@ -8,9 +6,7 @@ function App () {
   return (
     <div className='bg-custom1 mb-4'>
       <NavBar />
-      {/* <Bio /> */}
       <Resume />
-      <BodyNav />
     </div>
   )
 }
